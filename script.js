5309af1773b48044027576db7950ef5769b6e4d3
Ethan Edwards <ethancarteredwards@gmail.com>
2020-07-11 09:59:14 -0400

    nice nice

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
-// function EasyPeasyParallax() {
-// 	scrollPos = $(this).scrollTop();
-// 	$('.p1').css({
-// 		'background-position' : '50% ' + (-scrollPos/4)+"px"
-// 	});
-//   $('.p2').css({
-// 		'background-position' : '50% ' + (-scrollPos/8)+"px"
-// 	});
-//   $('.p3').css({
-// 		'background-position' : '70% ' + (-scrollPos/16)+"px"
-// 	});
-// 	$('.parallax-text').css({
-// 		'margin-top': (scrollPos/2)+"px",
-// 		'opacity': 1-(scrollPos/230)
-// 	});
+let quoteID = document.getElementById("quote")
+let authorID = document.getElementById("quoteAuthor")
+
+// let quote1 = "Once you start thinking more about where you want to be than about making the best product, you’re screwed."
+// let author1 = "- Linus Torvalds" 
+// let quote2 = "When exposing a crime is treated as commiting a crime, you are being ruled by criminals."
+// let author2 = "- Edward Snowden" 
+// let quote3 = "An idiot admires complexity, a genius admires simplicity."
+// let author3 = "- Terry A. Davis"
+
+let quotes = [
+    "Once you start thinking more about where you want to be than about making the best product, you’re screwed.",
+    "When exposing a crime is treated as commiting a crime, you are being ruled by criminals.",
+    "An idiot admires complexity, a genius admires simplicity."
+]
+let authors = [
+    "- Linus Torvalds",
+    "- Edward Snowden",
+    "- Terry A. Davis"
+]
+
+function getRandNum(max) {
+    return Math.floor(Math.random() * Math.floor(max))
+}
+number = getRandNum(3)
+
+quoteID.innerText = quotes[number]
+authorID.innerText = authors[number]
+// if (number == 0 ) {
+//     quoteID.innerText = quotes[0]
+//     authorID.innerHTML = authors[0]
+// } else if (number == 1) {
+//     quoteID.innerHTML = quotes[1]
+//     authorID.innerHTML = authors[1]
+// } else if (number == 2) {
+//     quoteID.innerHTML = quotes[2]
+//     authorID.innerHTML = authors[2]
 // }
-// $(document).ready(function(){
-// 	$(window).scroll(function() {
-// 		EasyPeasyParallax();
-// 	});
-// });
 
-// When the user scrolls the page, execute myFunction
-// window.onscroll = function() {myFunction()};
-
-// Get the header
-// var header = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-// var sticky = header.offsetTop;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-// function myFunction() {
-//   if (window.pageYOffset > sticky) {
-//     header.classList.add("sticky");
-//   } else {
-//     header.classList.remove("sticky");
-//   }
-// }
