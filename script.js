9383d50f8a7a86dce2b918daba0e9b9e4fb37e10
Ethan Edwards <ethancarteredwards@gmail.com>
2020-07-11 11:06:46 -0400

    it was a backtick oopos

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let quoteID = document.getElementById("quote")
 let authorID = document.getElementById("quoteAuthor")
 
 let quotes = [
-    "Once you start thinking more about where you want to be than about making the best product, you\’re screwed.",
+    "Once you start thinking more about where you want to be than about making the best product, you're screwed.",
     "Arguing that you don\'t care about privacy because you have nothing to hide is no different than saying you don\'t care about free speech because you have nothing to say." ,
     "An idiot admires complexity, a genius admires simplicity.",
     "The best way to predict the future is to create it.",
