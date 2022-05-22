d14652611534f1c23a7cec2caf7cf83c7d94571a
Ethan Edwards <ethan@ethancedwards.com>
2022-05-22 15:41:15 -0400

    move some tags

--- pages/_app.js
+++ pages/_app.js
@@ -13,6 +13,16 @@ export default function MyApp({ Component, pageProps }) {
         <div className="App">
           <Header />
 
+          <Head>
+
+            <meta charSet="utf-8" />
+
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="theme-color" content="#000000" />
+
+            <title>Ethan Carter Edwards</title>
+          </Head>
+
           <Component {...pageProps} />
         </div>
     );
