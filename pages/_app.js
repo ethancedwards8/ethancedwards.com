14fad3376f51d5078f8642d902182b6280cf7fa2
Ethan Edwards <ethan@ethancedwards.com>
2024-12-25 21:07:10 -0500

    add mostodon verification tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,30 @@
 import Head from 'next/head';
 
 import Header from '../components/header.js';
-import Footer from '../components/footer.js';
 
 import '../styles/all.scss';
 
 export default function MyApp({ Component, pageProps }) {
     return (
-        <div className="App">
-          <Header />
-
-          <Head>
-
-            <meta charSet="utf-8" />
-
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <meta name="theme-color" content="#000000" />
-
-            <title>Ethan Carter Edwards</title>
-          </Head>
-
-          <Component {...pageProps} />
-
-        </div>
+        <>
+            <a rel="me" href="https://fosstodon.org/@ethancedwards"></a>
+            <div className="App">
+              <Header />
+    
+              <Head>
+    
+                <meta charSet="utf-8" />
+    
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="theme-color" content="#000000" />
+                <link rel="me" href="https://fosstodon.org/@ethancedwards" />
+    
+                <title>Ethan Carter Edwards</title>
+              </Head>
+    
+              <Component {...pageProps} />
+    
+            </div>
+        </>
     );
 }
