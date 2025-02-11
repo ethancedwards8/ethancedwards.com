a06a0e7a7fd9d732815f7e17abd1b12daef2c990
Ethan Carter Edwards <ethan@ethancedwards.com>
2025-02-11 14:48:48 -0500

    jobs page MVP

---
+++ components/jobcard.js
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+import styles from '../styles/jobs.module.scss';
+
+export default function JobCard({ job }) {
+    return (
+        <>
+            <div className={styles.jobInfo}>
+                <Link href={job.job_url}><a><h2>{job.title}</h2></a></Link>
+                <h3>{job.company}</h3>
+                <h4>{job.location}</h4>
+                <h4>Date Posted: {job.date_posted}</h4>
+            </div>
+        </>
+    )
+}
